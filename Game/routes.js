28f809968bb72b57da359015ea2a153fdbe11622
Jaedon <jsballeyne@optimum.net>
2019-08-31 21:00:32 -0400

    last minute polish

--- Game/routes.js
+++ Game/routes.js
@@ -52,8 +52,8 @@ app.post("/login/", (req, res)=>{
         client.close()
       }else{
         return res.status(400).send({
-   message: 'no'
-});
+          message: 'no'
+        });
       }
     });
   })
@@ -67,38 +67,38 @@ app.post("/account/", (req, res)=>{
   let cpassword = req.body.CPassword;
   if(username.length <= 15 && password === cpassword){
 
-  MongoClient.connect(uri, (err, client) => {
-    if (err) {
-      console.error(err)
-      return
-    }
-    const db = client.db('DuelingMages')
-    let collection = db.collection('users')
-    collection.find({"Username": username}).toArray((err, result)=>{
-      if(result.length){
-        return res.status(400).send({
-        message: 'Name taken'
-        });
-      }else{
-        collection.insertOne({"Username": username, "Password": password}, (err, result)=> {
-          console.log(result);
-          if(result.length){
-            res.send("Made")
-            client.close()
-          }else{
-            return res.status(400).send({
-       message: 'server error'
-    });
-          }
-        })
+    MongoClient.connect(uri, (err, client) => {
+      if (err) {
+        console.error(err)
+        return
       }
+      const db = client.db('DuelingMages')
+      let collection = db.collection('users')
+      collection.find({"Username": username}).toArray((err, result)=>{
+        if(result.length){
+          return res.status(400).send({
+            message: 'Name taken'
+          });
+        }else{
+          collection.insertOne({"Username": username, "Password": password}, (err, result)=> {
+            console.log(result);
+            if(result.length){
+              res.send("Made")
+              client.close()
+            }else{
+              return res.status(400).send({
+                message: 'server error'
+              });
+            }
+          })
+        }
+      })
     })
-  })
-}else{
-  return res.status(400).send({
-message: 'check fields'
-});
-}
+  }else{
+    return res.status(400).send({
+      message: 'check fields'
+    });
+  }
 })
 
 app.post("/waiting-for-game/", (req, res)=>{
@@ -119,8 +119,8 @@ app.post("/waiting-for-game/", (req, res)=>{
         console.log("almost")
         console.log(result)
         return res.status(400).send({
-   message: 'already here'
-});
+          message: 'already here'
+        });
       }else{
         console.log("algameS")
         res.send("Yay")
@@ -132,187 +132,187 @@ app.post("/waiting-for-game/", (req, res)=>{
   })
 })
 
-  app.get("/logout/", (req, res)=>{
-    req.session.id = null;
-    req.session.Username = null;
-  })
+app.get("/logout/", (req, res)=>{
+  req.session.id = null;
+  req.session.Username = null;
+})
 
-  app.get("/waiting-for-game/", (req, res)=>{
-    if(req.session.id){
-      res.sendFile(path.join(__dirname + '/waiting-for-game.html'))
-    }
-  })
+app.get("/waiting-for-game/", (req, res)=>{
+  if(req.session.id){
+    res.sendFile(path.join(__dirname + '/waiting-for-game.html'))
+  }
+})
 
-  app.get("/wfglist/", (req, res)=>{
-    if(req.session.id){
-      MongoClient.connect(uri, (err, client) => {
-        if (err) {
-          console.error(err)
-          return
-        }
-        const db = client.db('DuelingMages')
-        let wfg = db.collection('wfg')
-        wfg.find({}).toArray((err, result)=> {
-          console.log(result);
-          if(result){
-            console.log("yeas")
-            //fix redirect
-            res.send([result, "DuelingMagesSOCOOLVERYHOTWOWIEWOWOMGGOD1LOLOLOOLO"])
-            client.close()
-          }
-        });
-      })
-    }
-  })
-
-  app.get("/DuelingMagesHost/", (req, res)=>{
-    res.sendFile(path.join(__dirname + '/index.html'))
-    console.log("GAMETIME")
-  })
-
-  app.get("/DuelingMagesRider/", (req, res)=>{
-    res.sendFile(path.join(__dirname + '/rider.html'))
-    console.log("GAMETIME")
-  })
-
-  app.post("/p2pid/", (req, res)=>{
-    let username = req.body.Username;
-    let p2pid = req.body.p2pid;
+app.get("/wfglist/", (req, res)=>{
+  if(req.session.id){
     MongoClient.connect(uri, (err, client) => {
       if (err) {
         console.error(err)
         return
       }
       const db = client.db('DuelingMages')
-      let wfg = db.collection('p2pid')
-      //add delete
-      wfg.deleteMany({"Username": req.session.Username}, (err, obj)=>{
-        if(err){
-          return
-        }else{
-          wfg.find({_id: req.session.id, "Username": req.session.Username}).toArray((err, result)=>{
-            if(result.length){
-              console.log("almost")
-              console.log(result)
-              return
-            }else{
-              console.log("algameS")
-              res.send("Yay")
-              wfg.insertOne({"Username": username, "p2pid":p2pid}, (err, res)=>{
-                client.close();
-              })
-            }
-          })
+      let wfg = db.collection('wfg')
+      wfg.find({}).toArray((err, result)=> {
+        console.log(result);
+        if(result){
+          console.log("yeas")
+          //fix redirect
+          res.send([result, "DuelingMagesSOCOOLVERYHOTWOWIEWOWOMGGOD1LOLOLOOLO"])
+          client.close()
         }
-      })
+      });
     })
-  })
+  }
+})
 
+app.get("/DuelingMagesHost/", (req, res)=>{
+  res.sendFile(path.join(__dirname + '/index.html'))
+  console.log("GAMETIME")
+})
 
-  app.post("/p2plist/", (req, res)=>{
-    if(req.session.id){
-      MongoClient.connect(uri, (err, client) => {
-        if (err) {
-          console.error(err)
-          return
-        }
-        const db = client.db('DuelingMages')
-        let wfg = db.collection('p2pid')
-        wfg.find({"Username": req.body.Username}).toArray((err, result)=> {
-          console.log(result);
+app.get("/DuelingMagesRider/", (req, res)=>{
+  res.sendFile(path.join(__dirname + '/rider.html'))
+  console.log("GAMETIME")
+})
+
+app.post("/p2pid/", (req, res)=>{
+  let username = req.body.Username;
+  let p2pid = req.body.p2pid;
+  MongoClient.connect(uri, (err, client) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    const db = client.db('DuelingMages')
+    let wfg = db.collection('p2pid')
+    //add delete
+    wfg.deleteMany({"Username": req.session.Username}, (err, obj)=>{
+      if(err){
+        return
+      }else{
+        wfg.find({_id: req.session.id, "Username": req.session.Username}).toArray((err, result)=>{
           if(result.length){
-            console.log("yeas")
-            //fix redirect
-            res.send([result])
-            client.close()
-          }
-        });
-      })
-    }
-  })
-
-  app.get("/p2pdelete/", (req, res)=>{
-    MongoClient.connect(uri, (err, client) => {
-      if (err) {
-        console.error(err)
-        return
-      }
-      const db = client.db('DuelingMages')
-      let wfg = db.collection('p2pid')
-      //add delete
-      wfg.deleteMany({"Username": req.session.Username}, (err, obj)=>{
-        if(err){
-          return
-        }else{
-          res.send("YAY")
-        }
-          })
-      })
-    })
-
-    app.post("/wfgdelete/", (req, res)=>{
-      let p2pid = req.body.p2pid;
-      MongoClient.connect(uri, (err, client) => {
-        if (err) {
-          console.error(err)
-          return
-        }
-        const db = client.db('DuelingMages')
-        let wfg = db.collection('wfg')
-        //add delete
-        console.log(p2pid)
-        wfg.deleteMany({"p2pid": p2pid}, (err, obj)=>{
-          if(err){
+            console.log("almost")
+            console.log(result)
             return
           }else{
-            res.send("YAY")
-          }
+            console.log("algameS")
+            res.send("Yay")
+            wfg.insertOne({"Username": username, "p2pid":p2pid}, (err, res)=>{
+              client.close();
             })
-        })
-      })
-
-      app.get("/loading/", (req, res)=>{
-        MongoClient.connect(uri, (err, client) => {
-          if (err) {
-            console.error(err)
-            return
           }
-          const db = client.db('DuelingMages')
-          let wfg = db.collection('load')
-          //add delete
-          wfg.insertOne({"Username": req.session.Username}, (err, obj)=>{
-            if(err){
-              return
-            }else{
-              res.send("YAY")
-            }
-              })
-          })
         })
-
-        app.post("/loading/", (req, res)=>{
-          let username = req.body.Username
-          MongoClient.connect(uri, (err, client) => {
-            if (err) {
-              console.error(err)
-              return
-            }
-            const db = client.db('DuelingMages')
-            let wfg = db.collection('load')
-            //add delete
-            wfg.deleteMany({"Username": username}, (err, obj)=>{
-              if(err){
-                return
-              }else{
-                res.send("YAY")
-              }
-                })
-            })
-          })
-
-
-  app.get("/", (req, res)=>{
-    res.sendFile(path.join(__dirname + '/login.html'))
-    console.log("here")
+      }
+    })
   })
-  app.listen(3000);
+})
+
+
+app.post("/p2plist/", (req, res)=>{
+  if(req.session.id){
+    MongoClient.connect(uri, (err, client) => {
+      if (err) {
+        console.error(err)
+        return
+      }
+      const db = client.db('DuelingMages')
+      let wfg = db.collection('p2pid')
+      wfg.find({"Username": req.body.Username}).toArray((err, result)=> {
+        console.log(result);
+        if(result.length){
+          console.log("yeas")
+          //fix redirect
+          res.send([result])
+          client.close()
+        }
+      });
+    })
+  }
+})
+
+app.get("/p2pdelete/", (req, res)=>{
+  MongoClient.connect(uri, (err, client) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    const db = client.db('DuelingMages')
+    let wfg = db.collection('p2pid')
+    //add delete
+    wfg.deleteMany({"Username": req.session.Username}, (err, obj)=>{
+      if(err){
+        return
+      }else{
+        res.send("YAY")
+      }
+    })
+  })
+})
+
+app.post("/wfgdelete/", (req, res)=>{
+  let p2pid = req.body.p2pid;
+  MongoClient.connect(uri, (err, client) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    const db = client.db('DuelingMages')
+    let wfg = db.collection('wfg')
+    //add delete
+    console.log(p2pid)
+    wfg.deleteMany({"p2pid": p2pid}, (err, obj)=>{
+      if(err){
+        return
+      }else{
+        res.send("YAY")
+      }
+    })
+  })
+})
+
+app.get("/loading/", (req, res)=>{
+  MongoClient.connect(uri, (err, client) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    const db = client.db('DuelingMages')
+    let wfg = db.collection('load')
+    //add delete
+    wfg.insertOne({"Username": req.session.Username}, (err, obj)=>{
+      if(err){
+        return
+      }else{
+        res.send("YAY")
+      }
+    })
+  })
+})
+
+app.post("/loading/", (req, res)=>{
+  let username = req.body.Username
+  MongoClient.connect(uri, (err, client) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    const db = client.db('DuelingMages')
+    let wfg = db.collection('load')
+    //add delete
+    wfg.deleteMany({"Username": username}, (err, obj)=>{
+      if(err){
+        return
+      }else{
+        res.send("YAY")
+      }
+    })
+  })
+})
+
+
+app.get("/", (req, res)=>{
+  res.sendFile(path.join(__dirname + '/login.html'))
+  console.log("here")
+})
+app.listen(3000);
